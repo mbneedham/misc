c8d258eb356112c6e7b8c50fa50c04f317c9b99c
Mark Needham <mbneedham@gmail.com>
2012-02-09 11:24:37 -0500

    small changes to perm.c

--- perm.c
+++ perm.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <string.h>
+#include <assert.h>
 
 void helper(char* s, int i){
   if (s[i] == '\0'){
     printf("%s\n", s);
   }else{
-    for (int j = i; j < strlen(s); j++){
+    int l = strlen(s);
+    for (int j = i; j < l; j++){
       char temp = s[i];
       s[i] = s[j];
       s[j] = temp;
@@ -19,6 +21,7 @@ void helper(char* s, int i){
 }
 
 void generate_permutations(const char* input_string){
+  assert(strlen(input_string) < 50);
   char s[50];
   strcpy(s, input_string);
   helper(s, 0);
@@ -28,8 +31,6 @@ void generate_permutations(const char* input_string){
 int main(int argc, char *argv[]){
   if (argc > 1){
     generate_permutations(argv[1]);
-  }else{
-    generate_permutations("Eyjafjallajokull");
   }
   return 1;
 }
